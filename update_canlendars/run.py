46e0c1e3669346b06bdab03b57019a7458126b4b
furuiyang <furuiyang0715@gmail.com>
2019-07-12 10:49:06 +0800

    配置 logbook

--- update_canlendars/run.py
+++ update_canlendars/run.py
@@ -1,14 +1,23 @@
+import sys
+
+from logbook import Logger, StreamHandler
+StreamHandler(sys.stdout).push_application()
+
+logger = Logger('Logbook')
 
 
 def calendars_update():
+    logger.info("开始更新交易日历")
     pass
 
 
 def index_update():
+    logger.info("开始更新指数数据")
     pass
 
 
 def finance_update():
+    logger.info("开始更新金融数据")
     pass
 
 
@@ -23,3 +32,8 @@ def update():
 
     # 对于金融数据的更新
     finance_update()
+
+
+if __name__ == "__main__":
+    update()
+    pass
